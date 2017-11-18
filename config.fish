f72954646c2f4a7cbc491a3af512907c32fb3870
nameLess_one <nameless_one@domain.org>
2017-11-18 03:29:15 +0200

    pyenv shims

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -U fish_key_bindings fish_vi_key_bindings
 # source ~/.iterm2_shell_integration.fish
-set PATH /Users/sysmanj/local/bin /Library/Frameworks/Python.framework/Versions/2.7/bin /System/Library/Frameworks/Python.framework/Versions/2.7/bin /Users/sysmanj/.cargo/bin /usr/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin /usr/local/MacGPG2/bin /usr/local/sbin $PATH
+set PATH (pyenv root)/shims /Users/sysmanj/local/bin /Library/Frameworks/Python.framework/Versions/2.7/bin /System/Library/Frameworks/Python.framework/Versions/2.7/bin /Users/sysmanj/.cargo/bin /usr/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin /usr/local/MacGPG2/bin /usr/local/sbin $PATH
 set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src/
 set -x OPENSSL_INCLUDE_DIR (brew --prefix openssl)/include
 set -x SNAPPY_LIB_DIR /usr/local/lib
