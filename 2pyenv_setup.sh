4f5e6b2377f7b2f7deb3e0b80fe39a7e31f9dd10
nameLess_one <nameless_one@domain.org>
2018-03-22 01:55:07 +0200

    bootstrap 3

---
+++ 2pyenv_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev
+sudo -u sysmanj curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
---
+++ 3pyenvironments_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pyenv install 3.6.4
+pyenv install 2.7.14
+pyenv virtualenv 2.7.14 neovim2
+pyenv virtualenv 3.6.4 neovim3
