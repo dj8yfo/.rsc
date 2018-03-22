dd347e1fd06e2092d6cb965fcc1d27a06072825b
nameLess_one <nameless_one@domain.org>
2018-03-22 20:28:02 +0200

    rg installation in essentials

--- 1essentials.sh
+++ 1essentials.sh
@@ -8,7 +8,8 @@ apt-get install task
 apt-get install git
 apt-get install xfonts-terminus console-terminus
 apt-get install ttf-liberation
-
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
+dpkg -i ripgrep_0.8.1_amd64.deb
 apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
