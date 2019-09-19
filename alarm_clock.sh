91fbd4adc2c365bb70de6ec5f73b16bc4783ec05
nameLess_one <nameless_one@domain.org>
2019-09-19 17:28:55 +0300

    alarm

---
+++ alarm_clock.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+cmus-remote --server /home/sysmanj/.config/cmus/socket -s
+cvlc ~/Desktop/alarm_clock_1.wav vlc://quit
+vlc vlc://quit
+cmus-remote --server /home/sysmanj/.config/cmus/socket -p
