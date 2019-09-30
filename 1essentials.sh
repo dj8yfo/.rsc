375ed51cb273a72e471bae8eacb767156e26e6ad
nameLess_one <nameless_one@domain.org>
2019-09-30 20:26:53 +0300

    essentials

--- 1essentials.sh
+++ 1essentials.sh
@@ -2,18 +2,17 @@
 set -e
 
 add-apt-repository ppa:neovim-ppa/unstable
-apt-get update
-apt-get install neovim
-apt-get install task
-apt-get install git
-apt-get install xfonts-terminus console-terminus
-apt-get install ttf-liberation
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install task
+sudo apt-get install git
+sudo apt-get install xfonts-terminus console-terminus
+sudo apt-get install ttf-liberation
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-dpkg -i ripgrep_0.8.1_amd64.deb
-apt-get install zsh
+sudo dpkg -i ripgrep_0.8.1_amd64.deb
+sudo apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
-cp .bashrc .xinitrc .xmodmap .zprofile .zshrc ~/
 ln -s $PWD/.bashrc $HOME/.bashrc
 ln -s $PWD/.xinitrc $HOME/.xinitrc
 ln -s $PWD/.xmodmap $HOME/.xmodmap
