f76a6a7e2bbdd9d1116694587d950be79d8706fe
nameLess_one <nameless_one@domain.org>
2019-10-01 11:30:38 +0300

    noevim python packagaes

--- 5nvim.sh
+++ 5nvim.sh
@@ -4,3 +4,15 @@ sudo -u sysmanj curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bi
 sudo -u sysmanj sh ./installer.sh ~/.local/share/dein
 sudo -u sysmanj rm -rf ~/.config/nvim
 sudo -u sysmanj git clone git@github.com:gisochre/.vimrc.git ~/.config/nvim
+
+pyenv activate neovim2
+pip install neovim
+pip install pynvim
+pip install jupyter
+pyenv deactivate
+
+pyenv activate neovim3
+pip install neovim
+pip install pynvim
+pip install jupyter
+pyenv deactivate
