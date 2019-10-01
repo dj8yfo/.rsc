de67bd971a53ed01faef49e31958d5d0fd3817de
nameLess_one <nameless_one@domain.org>
2019-10-01 23:24:09 +0300

    openvpn

---
+++ 8openvpn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo -s
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
+echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+sudo apt update
+sudo apt-get install -y openvpn
+sudo apt-get install -y network-manager-openvpn-gnome
