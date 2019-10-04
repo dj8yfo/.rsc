6eb2d201925b586c76f0af0d7005ac89a495fb7d
nameLess_one <nameless_one@domain.org>
2019-10-04 13:40:53 +0300

    username change

--- 5nvim.sh
+++ 5nvim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get install libboost-all-dev cmake libicu-dev
-sudo -u sysmanj curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sudo -u sysmanj sh ./installer.sh ~/.local/share/dein
-sudo -u sysmanj rm -rf ~/.config/nvim
-sudo -u sysmanj git clone git@github.com:gisochre/.vimrc.git ~/.config/nvim
+sudo -u hypen9 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sudo -u hypen9 sh ./installer.sh ~/.local/share/dein
+sudo -u hypen9 rm -rf ~/.config/nvim
+sudo -u hypen9 git clone git@github.com:gisochre/.vimrc.git ~/.config/nvim
 
 pyenv activate neovim2
 pip install neovim
