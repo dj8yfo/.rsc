cdb240472fb72c4c1de89cfdb0090ff34302ab16
nameLess_one <nameless_one@domain.org>
2019-10-13 19:58:52 +0300

    netkit install

---
+++ netkit-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dest=/tmp/netkit-install
+mkdir $dest
+pushd $dest
+wget http://wiki.netkit.org/download/netkit/netkit-2.8.tar.bz2
+wget http://wiki.netkit.org/download/netkit-filesystem/netkit-filesystem-i386-F5.2.tar.bz2
+wget http://wiki.netkit.org/download/netkit-kernel/netkit-kernel-i386-K2.8.tar.bz2
+tar -xjSf netkit-2.8.tar.bz2
+tar -xjSf netkit-filesystem-i386-F5.2.tar.bz2
+tar -xjSf netkit-kernel-i386-K2.8.tar.bz2
+mv ./netkit $NETKIT_HOME
+popd
+rm -rf $dest
