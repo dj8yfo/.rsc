30ccc0546a5157ab862a521fd0114ae0b5ebd15f
djeaunty8fourmule <sffhox@gmail.com>
2020-09-07 13:29:59 +0300

    taskwarrior configs mac

--- system_after_brick_wizard/resources/var-scripts/get_color_taskwar_tags.py
+++ system_after_brick_wizard/resources/var-scripts/get_color_taskwar_tags.py
@@ -4,7 +4,6 @@ import random
 import io
 import concurrent.futures
 
-
 def convert(uuid):
     res = []
     uuid = uuid.rstrip()
@@ -17,51 +16,52 @@ def convert(uuid):
             res.append(tag)
     return res
 
-print('Running tag generation for .taskwcolorrc...')
-all_unique_tags = set()
-compl_pr = subprocess.run(["task", "+ref", "uuids"], capture_output=True)
-uuids = compl_pr.stdout.decode().split()
+if __name__ == '__main__':
+    print('Running tag generation for .taskwcolorrc...')
+    all_unique_tags = set()
+    compl_pr = subprocess.run(["task", "+ref", "uuids"], capture_output=True)
+    uuids = compl_pr.stdout.decode().split()
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    for tag_res in executor.map(convert, uuids):
-        # print('new tags: %s' % tag_res)
-        all_unique_tags.update(tag_res)
-
-
-all_unique_tags.remove('ref')
-all_unique_tags = list(all_unique_tags)
-all_unique_tags = sorted(all_unique_tags, key=len, reverse=True)
-
-tags_file = f'{os.environ["HOME"]}/.unique_taskwarrior_tags'
-
-with open(tags_file, 'w') as tagf:
-    out = io.StringIO('')
-    for tag in all_unique_tags:
-        out.write(tag)
-        out.write('\n')
-    tagf.write(out.getvalue())
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for tag_res in executor.map(convert, uuids):
+            # print('new tags: %s' % tag_res)
+            all_unique_tags.update(tag_res)
 
 
-# task rule /credentials/ --> rgb200 match
-cfg_color_file=f'{os.environ["HOME"]}/.config/.taskwcolorrc'
-cfg_base_file=f'{os.environ["HOME"]}/Documents/.conf/base_color_rules'
-print(cfg_color_file)
-print(cfg_base_file)
-with open(cfg_base_file, 'r') as base_cfg:
-    base_rules = base_cfg.read()
+    all_unique_tags.remove('ref')
+    all_unique_tags = list(all_unique_tags)
+    all_unique_tags = sorted(all_unique_tags, key=len, reverse=True)
 
-# template = 'ms=40;38;5;{color_num}:sl=48;5;234	{tag}\\\\b\n'
-template = r'task rule /\b{tag}\b/ --> color{color_num} match'
-# template_rigid = 'ms=40;38;5;171:sl=48;5;234	~\\\\S+~\n'
-iter_color = 120
-with open(cfg_color_file, 'w') as cfg:
-    out = io.StringIO('')
-    for tag in all_unique_tags:
-        iter_color = random.randint(1, 255)
-        template_form = template.format(color_num=iter_color, tag=tag)
-        out.write(template_form)
-        out.write('\n')
-    cfg.write(base_rules)
-    cfg.write('\n')
-    cfg.write(out.getvalue())
+    tags_file = f'{os.environ["HOME"]}/.unique_taskwarrior_tags'
+
+    with open(tags_file, 'w') as tagf:
+        out = io.StringIO('')
+        for tag in all_unique_tags:
+            out.write(tag)
+            out.write('\n')
+        tagf.write(out.getvalue())
+
+
+    # task rule /credentials/ --> rgb200 match
+    cfg_color_file=f'{os.environ["HOME"]}/.config/.taskwcolorrc'
+    cfg_base_file=f'{os.environ["HOME"]}/Documents/.conf/base_color_rules'
+    print(cfg_color_file)
+    print(cfg_base_file)
+    with open(cfg_base_file, 'r') as base_cfg:
+        base_rules = base_cfg.read()
+
+    # template = 'ms=40;38;5;{color_num}:sl=48;5;234	{tag}\\\\b\n'
+    template = r'task rule /\b{tag}\b/ --> color{color_num} match'
+    # template_rigid = 'ms=40;38;5;171:sl=48;5;234	~\\\\S+~\n'
+    iter_color = 120
+    with open(cfg_color_file, 'w') as cfg:
+        out = io.StringIO('')
+        for tag in all_unique_tags:
+            iter_color = random.randint(1, 255)
+            template_form = template.format(color_num=iter_color, tag=tag)
+            out.write(template_form)
+            out.write('\n')
+        cfg.write(base_rules)
+        cfg.write('\n')
+        cfg.write(out.getvalue())
 
---
+++ system_after_brick_wizard_fucking_mac/k_taskwarrior_utils.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+source ./__prompt.sh
+prompt_install "taskwarrior install..."
+
+brew install ranger
+ranger --copy-config=all
+
+rm -rf $HOME/.config/ranger || true
+ln -s $PWD/subconfs/ranger $HOME/.config/ranger || true
+
+notes_dir=$HOME/Documents/code/tasking
+rm -rf $notes_dir || true
+git clone git@github.com:dj8yfo/tasking.git $notes_dir
+
+brew install task cpanminus clog
+rm -rf $HOME/.task
+ln -s $HOME/Documents/code/tasking/.task.d $HOME/.task
+rm -rf $HOME/tasknotes
+ln -s $HOME/Documents/code/tasking/.tasknotes.d $HOME/tasknotes
+
+rm -rf /tmp/taskopen || true
+git clone https://github.com/ValiValpas/taskopen.git /tmp/taskopen
+pushd /tmp/taskopen
+sudo make PREFIX=/usr/local install
+popd
+rm $HOME/.taskrc
+ln -s $PWD/taskconfigs/.taskrc $HOME/.taskrc
+
+rm $HOME/.taskopenrc
+ln -s $PWD/taskconfigs/.taskopenrc $HOME/.taskopenrc
+rm $HOME/.taskopenrc1
+ln -s $PWD/taskconfigs/.taskopenrc1 $HOME/.taskopenrc1
+sudo cpanm install JSON
+
+
+brew install vifm
+sudo cp ./resources/var-scripts/attach_vifm /usr/local/bin
+sudo chmod +x /usr/local/bin/attach_vifm
+
+sudo cp ./resources/var-scripts/xcat /usr/local/bin
+sudo cp ./resources/var-scripts/xbat /usr/local/bin
+sudo chmod +x /usr/local/bin/xcat
+
+mkdir -p $HOME/.config/tridactyl
+# ln -s $PWD/resources/tridactylrc $HOME/.config/tridactyl/tridactylrc
+sudo cp ./resources/var-scripts/gitpush /usr/local/bin
+sudo chmod +x /usr/local/bin/gitpush
